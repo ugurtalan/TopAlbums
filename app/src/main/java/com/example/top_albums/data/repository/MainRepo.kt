32f6022a1a454316aaae764942075efce55c2008
Uğur Talan <utalan@etu.edu.tr>
2026-01-13 16:54:13 +0300

    add repository

---
+++ app/src/main/java/com/example/top_albums/data/repository/MainRepo.kt
@@ -0,0 +1,28 @@
+package com.example.top_albums.data.repository
+
+import com.example.top_albums.data.remote.api.AlbumApi
+import com.example.top_albums.domain.model.MyAlbum
+
+class MainRepo (
+    private val api: AlbumApi
+)
+{
+    suspend fun getAlbums() : Result<List<MyAlbum>>{
+        return try {
+            val response = api.getTopAlbums()
+            val albums = response.feed.results.map { dto ->
+                MyAlbum(
+                    id = dto.id,
+                    name = dto.name,
+                    artist = dto.artistName,
+                    imageUrl = dto.artworkUrl100,
+                    appleUrl = dto.url
+                )
+            }
+            Result.success(albums)
+
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/top_albums/domain/model/MyAlbum.kt
@@ -0,0 +1,9 @@
+package com.example.top_albums.domain.model
+
+data class MyAlbum(
+    val id: String,
+    val name: String,
+    val artist: String,
+    val imageUrl: String,
+    val appleUrl: String
+)
