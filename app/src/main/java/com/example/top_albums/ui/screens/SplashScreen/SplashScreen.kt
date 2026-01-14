e65e98f1af0483fcccb0c0f8ff62884a7e8551f9
Uğur Talan <utalan@etu.edu.tr>
2026-01-14 23:21:36 +0300

    added splash screen

--- app/src/main/java/com/example/top_albums/ui/screens/SplashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/top_albums/ui/screens/SplashScreen/SplashScreen.kt
@@ -1,4 +1,41 @@
 package com.example.top_albums.ui.screens.SplashScreen
 
-class SplashScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun SplashScreen(
+    onContinueClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to Top Albums", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(onClick = { onContinueClick() },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.onSurface,
+
+            )) {
+            Text(text = "Go to List",
+                color = Color.White)
+        }
+    }
 }
